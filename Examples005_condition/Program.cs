7b06dbe994bc91b4372d2eb4a0290cd7850b38ea
Natalia Zhuravskaia <zlaska@yandex.ru>
2022-06-22 22:51:09 +0300

    Examples005 condition

---
+++ Examples005_condition/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+if( username.ToLower() == "маша")
+{
+    Console.WriteLine("ура, Маша!");
+}
+else 
+{
+    Console.WriteLine("ПРивет, ");
+    Console.WriteLine(username);
+}
